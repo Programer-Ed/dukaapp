4e839760a26638fd1a904a9445f916d671ff5597
Edwin <moyiedwin8@gmail.com>
2024-10-19 06:03:45 +0300

    edits to 10

--- seed.py
+++ seed.py
@@ -27,7 +27,7 @@ users = [{
     "name": faker.name(),
     "email": faker.unique.email(),  
     "password": faker.password(length=8, special_chars=True, digits=True, upper_case=True, lower_case=True)
-} for _ in range(100)]
+} for _ in range(10)]
 
 with app.app_context():
  
@@ -40,7 +40,7 @@ with app.app_context():
 
     products = []
     product_names = set() 
-    while len(products) < 100:
+    while len(products) < 10:
         product_name = faker.word()
         if product_name not in product_names:
             product_names.add(product_name)
@@ -59,22 +59,22 @@ with app.app_context():
                     "https://cdn.mafrservices.com/sys-master-root/hcc/h2b/16930286338078/9014_main.jpg",
                     "https://cdn.mafrservices.com/sys-master-root/h33/h3e/16930287353886/9006_main.jpg",
                     "https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/98/572685/1.jpg",
-                    "https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/81/0345581/1.jpg",
-                    "https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/75/070023/1.jpg",
-                    "https://cdn.mafrservices.com/sys-master-root/ha3/h4a/16871923089438/1294_main.jpg",
-                    "https://cdn.mafrservices.com/sys-master-root/h75/h9e/16930270969886/1494_main.jpg",
-                    "https://cdn.mafrservices.com/sys-master-root/hb2/hcc/16975373893662/43271_main.jpg",
-                    "https://cdn.mafrservices.com/sys-master-root/hed/h9a/51543114711070/211073_main.jpg",
-                    "https://img.freepik.com/premium-photo/bottle-milk-some-blueberries-table_662214-455559.jpg",
-                    "https://img.freepik.com/free-photo/three-bottles-beer_23-2148600945.jpg",
-                    "https://img.freepik.com/free-photo/backet-with-toilet-paper-rolls_23-2148550627.jpg",
-                    "https://img.freepik.com/premium-photo/three-eggs-brown-eggs-are-shown-with-water-droplets-them_978914-17758.jpg",
-                    "https://img.freepik.com/free-photo/ingredient-bags-full-flour_23-2149482567.jpg",
-                    "https://img.freepik.com/free-photo/black-white-cookies-package_140725-4381.jpg",
-                    "https://img.freepik.com/premium-photo/selection-artisanal-bread-rolls-including-whole-grain-ciabatta_1327465-40806.jpg",
-                    "https://img.freepik.com/premium-photo/stack-cheese-that-says-butter-it_1126605-1937.jpg",
-                    "https://img.freepik.com/premium-photo/dettol-soap-picture-dettol-soap-white-background-with-selective-focus_1197797-245618.jpg",
-                    "https://img.freepik.com/premium-photo/set-3d-bottles-with-oil_1299086-964.jpg"
+                    # "https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/81/0345581/1.jpg",
+                    # "https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/75/070023/1.jpg",
+                    # "https://cdn.mafrservices.com/sys-master-root/ha3/h4a/16871923089438/1294_main.jpg",
+                    # "https://cdn.mafrservices.com/sys-master-root/h75/h9e/16930270969886/1494_main.jpg",
+                    # "https://cdn.mafrservices.com/sys-master-root/hb2/hcc/16975373893662/43271_main.jpg",
+                    # "https://cdn.mafrservices.com/sys-master-root/hed/h9a/51543114711070/211073_main.jpg",
+                    # "https://img.freepik.com/premium-photo/bottle-milk-some-blueberries-table_662214-455559.jpg",
+                    # "https://img.freepik.com/free-photo/three-bottles-beer_23-2148600945.jpg",
+                    # "https://img.freepik.com/free-photo/backet-with-toilet-paper-rolls_23-2148550627.jpg",
+                    # "https://img.freepik.com/premium-photo/three-eggs-brown-eggs-are-shown-with-water-droplets-them_978914-17758.jpg",
+                    # "https://img.freepik.com/free-photo/ingredient-bags-full-flour_23-2149482567.jpg",
+                    # "https://img.freepik.com/free-photo/black-white-cookies-package_140725-4381.jpg",
+                    # "https://img.freepik.com/premium-photo/selection-artisanal-bread-rolls-including-whole-grain-ciabatta_1327465-40806.jpg",
+                    # "https://img.freepik.com/premium-photo/stack-cheese-that-says-butter-it_1126605-1937.jpg",
+                    # "https://img.freepik.com/premium-photo/dettol-soap-picture-dettol-soap-white-background-with-selective-focus_1197797-245618.jpg",
+                    # "https://img.freepik.com/premium-photo/set-3d-bottles-with-oil_1299086-964.jpg"
                 ])
             }
             products.append(product_data)
@@ -93,7 +93,7 @@ with app.app_context():
 
     user_ids = [user.id for user in User.query.all()]
     
-    for _ in range(50):
+    for _ in range(10):
         order_data = {
             "user_id": random.choice(user_ids),  
             "date": datetime.utcnow(),
@@ -110,7 +110,6 @@ with app.app_context():
 
     product_ids = [product.id for product in Product.query.all()]
 
-    # Create order items
     for order in orders:
         for _ in range(random.randint(1, 5)):
             order_item = OrderItem(
