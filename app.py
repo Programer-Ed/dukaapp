9c51302df6d65d784f203f93113aa40c24dcf95e
Edwin <moyiedwin8@gmail.com>
2024-11-04 11:11:29 +0300

    update CORS

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ app.json.compact = False
 allowed_origins = [
     "https://duka-moja.netlify.app",
     "https://admin-duka.netlify.app"
+    "https://duka-frontend.vercel.app"
+    "https://duka-admin.vercel.app"
 ]
 
 CORS(app, resources={r"/*": {"origins": allowed_origins}})
